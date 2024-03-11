97780132aad77a68fb436503e26ebf98d0477ae9
mikelisrael <israelipinkz@gmail.com>
2024-03-11 11:58:48 +0100

    Move login, layout and homepage components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Fredoka, Inter, Poppins } from "next/font/google";
-import "../globals.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
