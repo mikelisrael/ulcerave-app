a3de4aa5cd7a06325836b40e8acbde91056bc669
mikelisrael <israelipinkz@gmail.com>
2024-03-18 03:11:08 +0100

    Add pinkish color variable to globals.css and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,10 @@ const config = {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
+        pinkish: {
+          DEFAULT: "hsl(var(--pinkish))",
+          // foreground: "hsl(var(--pinkish-foreground))",
+        },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
