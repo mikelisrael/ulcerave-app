18e1ce2f8dfc5a58e03fdd9041f78d1c5dc32cbc
mikelisrael <israelipinkz@gmail.com>
2024-03-22 07:10:17 +0100

    Fix spacing in add-new/page.tsx

--- app/dashboard/patients/add-new/page.tsx
+++ app/dashboard/patients/add-new/page.tsx
@@ -4,7 +4,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 const page = () => {
   return (
     <ScrollArea className="h-full">
-      <div className="flex-1 space-y-4  p-4 pt-6 md:p-8">
+      <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
         <BreadCrumbDisplay />
       </div>
     </ScrollArea>
